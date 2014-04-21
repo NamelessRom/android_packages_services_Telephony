ea5ba1231f77950a43ed1e960b9417fed9e4b5ec
kaiyiz <kaiyiz@codeaurora.org>
2014-04-21 14:54:09 +0800

    TeleService: Remove the function of MO call on APM
    Remove the function of MO call on APM.
    
    CRs-Fixed: 651321
    
    Change-Id: Ifcef2e26a578243630c85174c163f63763f53db5

--- src/com/android/phone/CallController.java
+++ src/com/android/phone/CallController.java
@@ -273,7 +273,7 @@ public class CallController extends Handler {
                 // initiating an outgoing call, typically by directing the
                 // InCallScreen to display a diagnostic message (via the
                 // "pending call status code" flag.)
-                handleOutgoingCallError(status, intent);
+                handleOutgoingCallError(status);
                 break;
         }
 
@@ -644,10 +644,9 @@ public class CallController extends Handler {
      *
      * @param status one of the CallStatusCode error codes.
      */
-    private void handleOutgoingCallError(CallStatusCode status, Intent callIntent) {
+    private void handleOutgoingCallError(CallStatusCode status) {
         if (DBG) log("handleOutgoingCallError(): status = " + status);
-        String number = PhoneNumberUtils.getNumberFromIntent(callIntent, mApp);
-        Intent intent = new Intent(mApp, ErrorDialogActivity.class);
+        final Intent intent = new Intent(mApp, ErrorDialogActivity.class);
         int errorMessageId = -1;
         switch (status) {
             case SUCCESS:
@@ -727,7 +726,6 @@ public class CallController extends Handler {
                 break;
         }
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-        intent.putExtra(ErrorDialogActivity.CALL_NUMBER_EXTRA, number);
         if (errorMessageId != -1) {
             intent.putExtra(ErrorDialogActivity.ERROR_MESSAGE_ID_EXTRA, errorMessageId);
         }
